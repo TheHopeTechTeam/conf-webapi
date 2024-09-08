4cc620dc5220f413aa24e917bbabc5663094749b
Jay Hsia <jayhsia1997@gmail.com>
2024-09-07 23:39:54 -0400

    feat: Adjust urls

--- portal/urls.py
+++ portal/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
@@ -26,6 +27,7 @@ urlpatterns = [
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
     path('pages/', include(wagtail_urls)),
+    path("", redirect(to="cms")),
 ]
 
 if settings.DEBUG:
