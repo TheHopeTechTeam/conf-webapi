4105b34b0775dea7b7c659d4b3ab5273e4e7e9db
Jay Hsia <jayhsia1997@gmail.com>
2025-03-14 19:59:07 -0400

    feat(main): Add release workflow for Docker image build and service update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #exec gunicorn --graceful-timeout 30 --max-requests 100000 --max-requests-jitter 2000 -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 protal:app
-exec uvicorn portal:app --host 0.0.0.0 --port 8000 --workers 4 --timeout-keep-alive 60 --use-colors
+exec uvicorn portal:app --host 0.0.0.0 --port 8000 --timeout-keep-alive 60 --use-colors
