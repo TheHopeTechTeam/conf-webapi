38edcff52943a63bb4a543b3a3020e0e31f84001
Jay Hsia <jayhsia1997@gmail.com>
2025-04-06 12:22:18 -0400

    feat(main): Add created_at and updated_at fields to FCM Device model

--- portal/apps/fcm_device/models.py
+++ portal/apps/fcm_device/models.py
@@ -2,6 +2,7 @@
 FCM Device model
 """
 from django.db import models
+from django.utils import timezone
 from model_utils.models import UUIDModel
 from wagtail.search import index
 
@@ -14,6 +15,12 @@ class FCMDevice(index.Indexed, UUIDModel):
     token = models.CharField(max_length=255, unique=True)
     additional_data = models.JSONField(blank=True, null=True)
     accounts = models.ManyToManyField("account.Account", related_name="fcm_devices")
+    created_at = models.DateTimeField(
+        editable=False,
+        default=timezone.now,
+        db_comment="Creation timestamp"
+    )
+    updated_at = models.DateTimeField(editable=False, db_comment="Update timestamp", auto_now=True)
 
     def __str__(self):
         return self.device_id
@@ -22,3 +29,4 @@ class FCMDevice(index.Indexed, UUIDModel):
         db_table = "portal_fcm_device"
         verbose_name = "FCM Device"
         verbose_name_plural = "FCM Devices"
+        unique_together = ("device_id", "token")
